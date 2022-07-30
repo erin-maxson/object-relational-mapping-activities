ed4f5079a8cc2c1cf7f18851ae0a71a11732033d
erin-maxson <erin@cohatch.com>
2022-07-30 10:51:05 -0700

    updating activity 6

--- 06-Stu_Create-Read/Unsolved/routes/api/bookRoutes.js
+++ 06-Stu_Create-Read/Unsolved/routes/api/bookRoutes.js
@@ -2,23 +2,29 @@ const router = require('express').Router();
 const Book = require('../../models/Book');
 
 // TODO: Add a comment describing the purpose of this route
+//get book info
 router.get('/', (req, res) => {
   // TODO: Add a comment describing the functionality of this method
+  //find all books and then get the book data to return in json
   Book.findAll().then((bookData) => {
     res.json(bookData);
   });
 });
 
 // TODO: Add a comment describing the purpose of this route
+//getting the paperbacks model
 router.get('/paperbacks', (req, res) => {
   Book.findAll({
     // TODO: Add a comment describing the functionality of this property
+    //orders the paperbacks based on the title of the book
     order: ['title'],
     // TODO: Add a comment describing the functionality of this property
+    //makes sure that the only paperback data shown is from paperbacks and not hardback
     where: {
       is_paperback: true
     },
     attributes: {
+      //adds attributes of the paperback
       // TODO: Add a comment describing the functionality of this property
       exclude: ['is_paperback', 'edition']
     }
@@ -28,14 +34,17 @@ router.get('/paperbacks', (req, res) => {
 });
 
 // TODO: Add a comment describing the purpose of this route
+//gets the id of the data
 router.get('/:id', (req, res) => {
   // TODO: Add a comment describing the functionality of this method
+  //sets the parameters of the data based on the id
   Book.findByPk(req.params.id).then((bookData) => {
     res.json(bookData);
   });
 });
 
 // CREATE a book
+//posts one book or lists an error if the book doesn't have the information needed
 router.post('/', (req, res) => {
   Book.create(req.body)
     .then((newBook) => {
@@ -47,6 +56,7 @@ router.post('/', (req, res) => {
 });
 
 // CREATE multiple books
+//creats mulitple books based on the seed data
 router.post('/seed', (req, res) => {
   Book.bulkCreate([
     {
