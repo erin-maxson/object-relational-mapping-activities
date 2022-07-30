72e36b11c5f91f7a22304782f925f0bbf66b96b5
erin-maxson <erin@cohatch.com>
2022-07-30 09:53:35 -0700

    activity updates

--- 02-Stu_Sequelize-Setup/Unsolved/config/connection.js
+++ 02-Stu_Sequelize-Setup/Unsolved/config/connection.js
@@ -1,9 +1,11 @@
 const Sequelize = require('sequelize');
+require("dotenv").config()
 
 const sequelize = new Sequelize(
-  'library_db',
-  'root',
-  'myPassword',
+
+process.env.DB_DATABASE,
+process.env.DB_USERNAME,
+process.env.DB_PASSWORD,
   {
     host: 'localhost',
     dialect: 'mysql',
