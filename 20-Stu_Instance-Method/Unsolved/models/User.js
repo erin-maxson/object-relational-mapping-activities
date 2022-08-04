efb693eb99af2856509dba64202069208d37ede4
erin-maxson <erin@cohatch.com>
2022-08-03 17:52:20 -0700

    updates

--- 20-Stu_Instance-Method/Unsolved/models/User.js
+++ 20-Stu_Instance-Method/Unsolved/models/User.js
@@ -6,12 +6,9 @@ class User extends Model {
   checkPassword(userPassword) {
     // TODO: Complete the instance method
     // Load hash from your password DB.
-    bcrypt.compare(myPlaintextPassword, hash, function (err, result) {
-      // result == true
-    });
-    bcrypt.compare(someOtherPlaintextPassword, hash, function (err, result) {
-      // result == false
-    });
+    const password=bcrypt.compareSync(userPassword, this.password)
+
+    return password
 
   }
 }
